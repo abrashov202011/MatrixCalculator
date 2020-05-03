035be40b2269c77773515a825255ac7bea968f5b
Abrashov <abrashov20201@mail.ru>
2020-05-03 13:17:44 +0300

    Реальзован вывод основного фрейма приложения

---
+++ src/com/codebind/UI/MainPanel.java
@@ -0,0 +1,13 @@
+package com.codebind.UI;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainPanel extends JPanel {
+    /**
+     * Конструктор - создание нового объекта
+     */
+    public MainPanel() {
+        this.setLayout(new BorderLayout());
+    }
+}
