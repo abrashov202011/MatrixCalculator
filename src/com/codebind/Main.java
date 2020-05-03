ce9a18199d3ce94f3077a2ea7145f7abcdf0aadb
Abrashov <abrashov20201@mail.ru>
2020-05-03 13:18:50 +0300

    Реальзован вывод главной панели в которой будет отрисован весь интерфейс приложения

--- src/com/codebind/Main.java
+++ src/com/codebind/Main.java
@@ -1,4 +1,19 @@
 package com.codebind;
 
+import com.codebind.UI.MainPanel;
+
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
+    public static MainPanel mainPanel;
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(new Dimension(1000, 800));
+        mainPanel = new MainPanel();
+        frame.add(mainPanel);
+        frame.setVisible(true);
+    }
 }
